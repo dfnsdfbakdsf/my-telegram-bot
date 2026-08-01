7691c0a940456655e0db1ed79e863af430ec1324
dfnsdfbakdsf <dotka1272@mail.ru>
2026-08-01 12:52:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 # --- НАСТРОЙКИ ---
 BOT_TOKEN = '8935278169:AAFfVupDDFrp2rHufzQYJrsnWJxtI54Xxvw'  # ЗАМЕНИТЕ НА ВАШ ТОКЕН!
 GROUP_CHAT_ID = -1004291446609  # ID вашей группы/админа
-ADMIN_IDS = [123456789, 987654321]  # *** ДОБАВЬТЕ СЮДА СВОИ TELEGRAM ID ***
+ADMIN_IDS = [6805635660, 7334259357]  # *** ДОБАВЬТЕ СЮДА СВОИ TELEGRAM ID ***
 # -----------------
 
 bot = telebot.TeleBot(BOT_TOKEN)
